6d30feb3a685e82eb8a84d4757f5a69012c1d93c
Argestus <56864654+Argestus@users.noreply.github.com>
2019-12-13 11:06:48 +0200

    update

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+const DOMmaps = document.querySelector('.map')
+console.log(DOMmaps);
+
+let tile = 0;
+
+for (let i = 0; i < maps.length; i++) {
+    const mapRow = maps[i];
+        for (let j = 0; j < maps[i].length; j++) {
+            const tile = maps[i][j];
+            return tile; 
+        }
+}
+console.log(tile);
+
+// function generateMap(maps) {
+//     if (block === 1) {
+        
+//     } else {
+        
+//     }
+// }
